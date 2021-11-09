3c5336678b1de88cff68011a8eea5bdd55b16d69
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-09 13:41:02 -0300

    Sign up route

---
+++ src/controllers/account.js
@@ -0,0 +1,36 @@
+import bcrypt from 'bcrypt';
+import connection from '../database/database.js';
+import { signUpSchema } from '../../Validation/Schemes.js';
+
+// eslint-disable-next-line consistent-return
+async function signUp(req, res) {
+    const { name, email, password } = req.body;
+
+    if (signUpSchema.validate({ name, email, password }).error) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const result = await connection.query(
+            'SELECT * FROM users WHERE email = $1;',
+            [email],
+        );
+
+        if (result.rowCount) return res.sendStatus(409);
+
+        const passwordHash = bcrypt.hashSync(password, 10);
+
+        await connection.query(
+            'INSERT INTO users(name, email, password) VALUES ($1, $2, $3);',
+            [name, email, passwordHash],
+        );
+
+        res.sendStatus(201);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { signUp };
