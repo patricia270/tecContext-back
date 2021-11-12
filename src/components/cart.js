d6fea7cc7d8b4f27e6e001fb4b2a7207320b7d69
Gabriel Pires <gabriel.pires@twistsystems.com>
2021-11-12 16:12:31 -0300

    ✏️ Fix Typos ESlint

--- src/components/cart.js
+++ src/components/cart.js
@@ -1,70 +1,67 @@
-import connection from "../database/database.js";
-import { cartSchema } from "../../Validation/Schemes.js";
+import connection from '../database/database.js';
+import { cartSchema } from '../../Validation/Schemes.js';
 
-async function postCartItem (req,res) {
-    let {product_id, quantity, user_id } = req.body;
+async function postCartItem(req, res) {
+    let { product_id, quantity, user_id } = req.body;
     try {
-
-        const userResult = await connection.query(`SELECT * FROM users WHERE id = $1`,[user_id])
+        const userResult = await connection.query('SELECT * FROM users WHERE id = $1', [user_id]);
         const user = userResult.rows[0];
-        const prodResult = await connection.query(`SELECT * FROM products WHERE id = $1`,[product_id]);
+        const prodResult = await connection.query('SELECT * FROM products WHERE id = $1', [product_id]);
         const product = prodResult.rows[0];
-        const values = await cartSchema.validateAsync({quantity})    
+        await cartSchema.validateAsync({ quantity });
 
-        if (!product) return res.sendStatus(401)
-        if (!user) user_id = ''
-        if (product.stock_qtd < parseInt(quantity)) return res.sendStatus(400)
+        if (!product) return res.sendStatus(401);
+        if (!user) user_id = '';
+        if (product.stock_qtd < parseInt(quantity, 10)) return res.sendStatus(400);
 
-        const cartItem = await connection.query(`INSERT INTO cart (user_id,product_id,quantity) VALUES ($1,$2,$3)`, [user_id,product_id,quantity])
+        await connection.query('INSERT INTO cart (user_id,product_id,quantity) VALUES ($1,$2,$3)', [user_id, product_id, quantity]);
 
-        res.sendStatus(201)
+        return res.sendStatus(201);
     } catch (err) {
-        console.log(err)
-        res.sendStatus(401)
+        return res.sendStatus(401);
     }
 }
 
-async function getCartItems (req,res) {
+async function getCartItems(req, res) {
     let { id } = req.params;
 
     try {
-        const userSession = await connection.query(`SELECT * FROM sessions WHERE user_id = $1`, [String(id)])
-        if(!userSession.rows[0]) id = 0
+        const userSession = await connection.query('SELECT * FROM sessions WHERE user_id = $1', [String(id)])
+        if (!userSession.rows[0]) id = 0;
         //Will pass user_id if logged and '' as guest
-        const items = await connection.query(`SELECT * FROM cart WHERE user_id = $1`,[String(id)])
+        const items = await connection.query('SELECT * FROM cart WHERE user_id = $1', [String(id)]);
 
-        res.send(items.rows)
+        res.send(items.rows);
     } catch (err) {
-        console.log(err)
-        res.sendStatus(400)
+        res.sendStatus(400);
     }
 }
 
-async function changeCartItem(req,res) {
+async function changeCartItem(req, res) {
     let { id } = req.params;
-    const { product_id,quantity } = req.body;
+    const { product_id, quantity } = req.body;
     if (id == 0) {
-        id = ''
-    }else {
-        String(id)
+        id = '';
+    } else {
+        String(id);
     }
     try {
         if (quantity === 0) {
             await connection.query(`
                 DELETE FROM cart WHERE product_id = $1 AND user_id = $2
-            `, [product_id,id])
-            return res.sendStatus(201)
+            `, [product_id, id])ç
+            return res.sendStatus(201);
         }
         await connection.query(`
-            UPDATE cart SET quantity = $1 WHERE product_id = $2 AND user_id = $3`, [quantity, product_id, id])
+            UPDATE cart SET quantity = $1 
+            WHERE product_id = $2 
+            AND user_id = $3`, [quantity, product_id, id]);
 
-        return res.sendStatus(201)
-
-    } catch (err){
-        console.log(err)
-        res.sendStatus(err)
+        return res.sendStatus(201);
+    } catch (err) {
+        return res.sendStatus(err);
     }
-
 }
+
 
 export {postCartItem, getCartItems,changeCartItem}
