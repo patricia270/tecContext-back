d1cd50f6778a5f41da60a61f6811d77c79b87061
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-11-12 15:54:06 -0400

    update

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import { signIn, signUp } from './controllers/account.js';
-import { getCartItems, postCartItem,changeCartItem } from './components/cart.js'
+import { getCartItems, postCartItem, changeCartItem } from './components/cart.js';
 
 const app = express();
 
@@ -11,9 +11,8 @@ app.use(cors());
 app.post('/sign-up', signUp);
 app.post('/sign-in', signIn);
 
-app.post('/cart', postCartItem)
-app.put('/cart/:id', changeCartItem)
-app.get('/cart/:id', getCartItems)
-
+app.post('/cart', postCartItem);
+app.put('/cart/:id', changeCartItem);
+app.get('/cart/:id', getCartItems);
 
 export default app;
