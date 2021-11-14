0dbf6ac03c5f788d875450a4348fa8354c1a23a2
patriciasilva.eq@gmail.com <patriciasilva.eq@gmail.com>
2021-11-14 00:41:52 -0400

    Testing database Heroku

--- src/database/database.js
+++ src/database/database.js
@@ -8,6 +8,10 @@ let databaseConfig = ({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 if (process.env.NODE_ENV === 'prod') {
